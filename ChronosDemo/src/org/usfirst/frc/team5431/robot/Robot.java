258af42331fd984f7470b63fb1a3a71a26b4de3f
AcademyHSRobotics <AcademyHSRobotics@10.54.31.232>
2018-08-21 20:43:24 -0500

    works lol

--- ChronosDemo/src/org/usfirst/frc/team5431/robot/Robot.java
+++ ChronosDemo/src/org/usfirst/frc/team5431/robot/Robot.java
@@ -10,6 +10,7 @@ package org.usfirst.frc.team5431.robot;
 import edu.wpi.first.wpilibj.IterativeRobot;
 
 import edu.wpi.first.wpilibj.PWMTalonSRX;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 
@@ -26,10 +27,13 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
  * project.
  */
 public class Robot extends IterativeRobot {
+	private Titan.LogitechExtreme3D Logitech; 
 	private Titan.Xbox driverXbox;
 	private WPI_TalonSRX frontLeft, frontRight, rearLeft, rearRight,shootLeft,shootRight,intake;
 	private DifferentialDrive driveBase;
-	//private SpeedControllerGroup frontLeftGroup, frontRightGroup, rearLeftGroup, rearRightGroup;
+	private SpeedControllerGroup left, right,shooter; 
+	private Titan.Toggle isIntaking = new Titan.Toggle();
+
 	
 
 	/**
@@ -43,33 +47,51 @@ public class Robot extends IterativeRobot {
 	 * initializing all the variables in the project
 	 */
 	driverXbox = new Titan.Xbox(0);
+	Logitech = new Titan.LogitechExtreme3D(1); 
 	
 	driverXbox.setDeadzone(0.15);
 	
-	frontLeft = new WPI_TalonSRX(2);	//frontLeft.setInverted(true);
-	frontRight = new WPI_TalonSRX(8);	//frontRight.setInverted(true);
-	rearLeft = new WPI_TalonSRX(3);		//rearLeft.setInverted(true);
-	rearRight = new WPI_TalonSRX(7);	//rearRight.setInverted(true);
-	shootLeft = new WPI_TalonSRX(4);	//rearRight.setInverted(true);
+	frontLeft = new WPI_TalonSRX(2);	frontLeft.setInverted(true);
+	frontRight = new WPI_TalonSRX(8);	frontRight.setInverted(true);
+	rearLeft = new WPI_TalonSRX(3);		rearLeft.setInverted(true);
+	rearRight = new WPI_TalonSRX(7);	rearRight.setInverted(true);
+	shootLeft = new WPI_TalonSRX(4);	rearRight.setInverted(true);
 	shootRight = new WPI_TalonSRX(6); 
+	frontLeft.setInverted(true);
+	frontRight.setInverted(true);
+	rearLeft.setInverted(true);
+	rearRight.setInverted(true);
 	intake = new WPI_TalonSRX(5); 
-	
+	left = new SpeedControllerGroup(frontLeft,rearLeft);
+	right = new SpeedControllerGroup(frontRight,rearRight);
+	shooter = new SpeedControllerGroup(shootLeft,shootRight);
+	driveBase = new DifferentialDrive(left,right); 	
 }
-
-	@Override
-	public void teleopInit() {
-		// This makes sure that the autonomous stops running when
-		// teleop starts running. If you want the autonomous to
-		// continue until interrupted by another command, remove
-		// this line or comment it out.
-	}
 
 	/**
 	 * This function is called periodically during operator control.
-	 */
+	 */ 
 	@Override
 	public void teleopPeriodic() {
-		Scheduler.getInstance().run();
+		double leftY = driverXbox.getRawAxis(Titan.Xbox.Axis.LEFT_Y); 
+		double rightY = driverXbox.getRawAxis(Titan.Xbox.Axis.RIGHT_Y); 
+		double shooterY = Logitech.getRawAxis(Titan.LogitechExtreme3D.Axis.Y); 
+		boolean boolIntake = Logitech.getRawButton(1); 
+		boolean boolOutake = Logitech.getRawButton(2); 
+		driveBase.tankDrive(leftY, rightY);
+		shooter.set(shooterY);
+		if(isIntaking.isToggled(boolIntake)) {
+			intake.set(0.6);
+		}
+		else if(boolOutake) {
+			intake.set(-0.6);
+		}
+		else {
+			intake.set(0);
+		}
+		
+		
+		
 	}
 
 	/**
