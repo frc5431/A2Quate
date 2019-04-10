86372b96c40a0047c88a875fa7b2300478241dda
Tauseef Afraz <43244309+TFM110@users.noreply.github.com>
2019-04-10 10:44:24 -0500

    Update Dashboard.java

--- Talon/2019Ad2quate/src/main/java/frc/robot/components/Dashboard.java
+++ Talon/2019Ad2quate/src/main/java/frc/robot/components/Dashboard.java
@@ -1,1 +1,17 @@
+package frc.robot.components;
 
+import frc.robot.*;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+
+public class Dashboard {
+    
+  public Dashboard() {
+       final CameraServer cameraServer = CameraServer.getInstance();
+
+       final UsbCamera FrontCam = cameraServer.startAutomaticCapture("FrontCam", 1);
+       final UsbCamera BackCam = cameraServer.startAutomaticCapture("BackCam", 0);
+  }
+}
